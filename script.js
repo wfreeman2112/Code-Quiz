6ff68271dc3d23c86f153c5e13f7b390a137aa42
Wendi Freeman <wfreeman2112@gmail.com>
2020-10-20 12:20:26 -0500

    completed questionnaire in JS

--- script.js
+++ script.js
@@ -1,31 +1,113 @@
-/* create element to store questions
-    create element to store answers
-    show the questions
-    when user clicks submit, show results
-    make timer 
-    decrement time from clock if answer is wrong
-    when all questions are answered OR timer = 0, stop game
-    save initials and score
-*/
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
 
-var questions;
-questions = ['question 1', 'question 2', 'question 3', 'question 4', 'question 5'];
+let shuffledQuestions, currentQuestionIndex
 
-//build seperate arrays for answers to each question?
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-var startButton = document.querySelector('.start')
-
-function startTimer(){
-    var secondsRemaining = userInput.value
-    var timerInterval = setInterval(function(){
-         secondsRemaining-- 
-            timeRemainingSection.textContent = secondsRemaining
-
-        if (secondsRemaining === 0){
-            clearInterval(timerInterval)   
-        }
-    }, 1000)
-
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-startButton.addEventListener('click', startTimer)
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'question1',
+    answers: [
+      { text: 'answer1', correct: true },
+      { text: 'answer2', correct: false },
+      { text: 'answer3', correct: false }
+    ]
+  },
+  {
+    question: 'question2',
+    answers: [
+      { text: 'answer1', correct: false },
+      { text: 'answer2', correct: false },
+      { text: 'answer3', correct: true }
+    ]
+  },
+  {
+    question: 'question3',
+      answers: [
+        { text: 'answer1', correct: false },
+        { text: 'answer2', correct: false },
+        { text: 'answer3', correct: true }
+      ]
+    },
+  
+    {
+      question: 'question4',
+      answers: [
+        { text: 'answer1', correct: false },
+        { text: 'answer2', correct: true },
+        { text: 'answer3', correct: false }
+      ]
+    }
+]
